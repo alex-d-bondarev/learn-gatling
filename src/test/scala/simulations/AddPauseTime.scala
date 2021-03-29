1c4253add911775efa7f6cec7e9a969809d6167f
Alex <alex.d.bondarev@gmail.com>
2021-03-29 22:00:38 +0100

    add repeats

--- src/test/scala/simulations/AddPauseTime.scala
+++ src/test/scala/simulations/AddPauseTime.scala
@@ -7,30 +7,36 @@ import scala.concurrent.duration._
 
 class AddPauseTime extends Simulation {
 
-  val httpConf = http.baseUrl("http://127.0.0.1:5000")
+  private val httpConf = http.baseUrl("http://127.0.0.1:5000")
     .header("Accept", "application/json")
 
-  val scn = scenario("Flask Simple App")
-    .exec(
-      http("Get Simple App About call")
-        .get("/about")
-        .check(status.is(200)))
-    .pause(3)
-
-    .exec(
-      http("Navigate to Index page")
-        .get("/about")
-        .check(status.is(200)))
-    .pause(2000.milliseconds)
-
-    .exec(
-      http("Navigate to Hello page")
-        .get("/about")
-        .check(status.is(200)))
-    .pause(1, 4)
+  private val scn = scenario("Flask Simple App")
+    .exec(getAboutCall()).pause(500.milliseconds)
+    .exec(getIndexCall()).pause(2000.milliseconds)
+    .exec(getHelloCall()).pause(1, 3)
+    .exec(getAboutCall()).pause(200.milliseconds)
+    .exec(getIndexCall()).pause(100.milliseconds)
 
   setUp(
     scn.inject(atOnceUsers(1))
   ).protocols(httpConf)
+
+  private def getAboutCall() = exec(
+    http("Get Simple App About call")
+      .get("/about")
+      .check(status.is(200)))
+
+  private def getIndexCall() = exec(
+    http("Navigate to Index page")
+      .get("/about")
+      .check(status.is(200)))
+
+  private def getHelloCall() =
+    repeat(2) {
+      exec(
+        http("Get Simple App About call")
+          .get("/about")
+          .check(status.is(200)))
+    }
 
 }
